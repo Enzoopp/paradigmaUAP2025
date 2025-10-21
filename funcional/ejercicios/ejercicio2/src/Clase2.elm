70912eeb5c87124993fb04824e198900fed910e3
Enzo Pitana <enzopitana@gmail.com>
2025-10-21 17:05:38 -0300

    Update Clase2.elm

--- funcional/ejercicios/ejercicio2/src/Clase2.elm
+++ funcional/ejercicios/ejercicio2/src/Clase2.elm
@@ -18,22 +18,7 @@ isEmpty list =
     List.isEmpty list
 
 
-{-| Ejercicios de Programación Funcional - Clase 2
-Este módulo contiene ejercicios para practicar conceptos de programación funcional en Elm.
-Cada función debe implementarse usando principios de programación funcional.
-
-Nota: Las funciones que podrían fallar (como buscar elementos) devuelven valores por defecto (0)
-en lugar de usar Maybe. Trabajamos con List en lugar del List de Scala.
-
--}
-
-
-
 -- Concatenar
--- Dadas 2 listas, devuelve una lista que es la concatenación de ambas.
--- Usar recursión
-
-
 concatenar : List Int -> List Int -> List Int
 concatenar lista1 lista2 =
     if isEmpty lista1 then 
@@ -43,10 +28,6 @@ concatenar lista1 lista2 =
 
 
 -- Buscar
--- Dada una lista y una función de comparación, devuelve el valor que cumple la condición.
--- Devuelve 0 si la lista está vacía.
-
-
 buscar : List Int -> (Int -> Int -> Bool) -> Int
 buscar lista com =
     if isEmpty lista then 
@@ -65,104 +46,45 @@ buscar lista com =
 
 
 -- Busca el Máximo
--- Encuentra el valor máximo en una lista
-
-
 max : List Int -> Int
 max lista =
-    if isEmpty lista then 
-        0
-    else if isEmpty (tail lista) then 
-        head lista
-    else 
-        let
-            h = head lista
-            m = max (tail lista)
-        in
-        if h > m then 
-            h
-        else 
-            m
+    buscar lista (>)
 
 
 -- Busca el Mínimo
--- Encuentra el valor mínimo en una lista
-
-
 min : List Int -> Int
 min lista =
-    if isEmpty lista then 
-        0
-    else if isEmpty (tail lista) then 
-        head lista
-    else 
-        let
-            h = head lista
-            m = min (tail lista)
-        in
-        if h < m then 
-            h
-        else 
-            m
+    buscar lista (<)
 
 
 -- Filtra la lista de valores mayores que el valor e pasado por parámetro
-
-
 maximos : List Int -> Int -> List Int
 maximos lista e =
-    if isEmpty lista then
-        []
-    else
-        let
-            h = head lista
-            resto = maximos (tail lista) e
-        in
-        if h > e then
-            h :: resto
-        else
-            resto
+    filtrar lista (\x -> x > e)
 
 
 -- Filtra la lista de valores menores que el valor e pasado por parámetro
-
-
 minimos : List Int -> Int -> List Int
 minimos lista e =
-    if isEmpty lista then
-        []
-    else
-        let
-            h = head lista
-            resto = minimos (tail lista) e
-        in
-        if h < e then
-            h :: resto
-        else
-            resto
+    filtrar lista (\x -> x < e)
 
 
 -- Ordena los valores de una lista utilizando quicksort
-
-
 quickSort : List Int -> List Int
 quickSort xs =
-    case xs of
-        [] ->
-            []
-
-        pivot :: resto ->
-            let
-                menores = minimos resto pivot
-                mayores = maximos resto (pivot - 1)
-            in
-            concatenar (concatenar (quickSort menores) [pivot]) (quickSort mayores)
+    if isEmpty xs then
+        []
+    else
+        let
+            pivot = head xs
+            resto = tail xs
+            menores = filtrar resto (\x -> x <= pivot)
+            mayores = filtrar resto (\x -> x > pivot)
+        in
+        concatenar (concatenar (quickSort menores) [pivot]) (quickSort mayores)
 
 
 -- Obtiene un elemento en la posición n (empezando desde 0)
--- Devuelve 0 si la posición está fuera de rango
-
-
 obtenerElemento : List Int -> Int -> Int
 obtenerElemento lista posicion =
     if posicion < 0 then 
@@ -176,11 +98,6 @@ obtenerElemento lista posicion =
 
 
 -- Busca la mediana
--- En el ámbito de la estadística, la mediana representa el
--- valor de la variable de posición central en un conjunto de datos ordenados.
--- Devuelve 0 si la lista está vacía.
-
-
 mediana : List Int -> Int
 mediana lista =
     let
@@ -195,8 +112,6 @@ mediana lista =
 
 
 -- Cuenta los elementos
-
-
 contar : List Int -> Int
 contar lista =
     if isEmpty lista then
@@ -206,8 +121,6 @@ contar lista =
 
 
 -- Acumula los elementos
-
-
 acc : List Int -> Int
 acc lista =
     if isEmpty lista then
@@ -217,8 +130,6 @@ acc lista =
 
 
 -- Filtra los elementos de la lista xs según la función p
-
-
 filtrar : List Int -> (Int -> Bool) -> List Int
 filtrar xs p =
     if isEmpty xs then
@@ -235,24 +146,18 @@ filtrar xs p =
 
 
 -- Filtra los elementos pares usando la función filtrar
-
-
 filtrarPares : List Int -> List Int
 filtrarPares xs =
     filtrar xs (\x -> modBy 2 x == 0)
 
 
 -- Filtra los elementos múltiplos de 3 usando filtrar
-
-
 filtrarMultiplosDeTres : List Int -> List Int
 filtrarMultiplosDeTres xs =
     filtrar xs (\x -> modBy 3 x == 0)
 
 
 -- Acumula los elementos aplicándoles fx
-
-
 acumular : List Int -> (Int -> Int) -> Int
 acumular lista fx =
     if isEmpty lista then
@@ -261,34 +166,25 @@ acumular lista fx =
         fx (head lista) + acumular (tail lista) fx
 
 
--- Acumula todos los elementos de una lista usando acumular (función identidad)
-
-
+-- Acumula todos los elementos de una lista usando acumular
 acumularUnidad : List Int -> Int
 acumularUnidad lista =
     acumular lista (\x -> x)
 
 
 -- Acumula el doble de los elementos de una lista usando acumular
-
-
 acumularDoble : List Int -> Int
 acumularDoble lista =
     acumular lista (\x -> x * 2)
 
 
 -- Acumula el cuadrado de los elementos de una lista usando acumular
-
-
 acumularCuadrado : List Int -> Int
 acumularCuadrado lista =
     acumular lista (\x -> x * x)
 
 
 -- Transforma la lista a una lista de otro tipo
--- Esto es equivalente a la función map de Scala
-
-
 transformar : List Int -> (Int -> a) -> List a
 transformar lista fx =
     if isEmpty lista then
@@ -298,8 +194,6 @@ transformar lista fx =
 
 
 -- Retorna true si un elemento existe en la lista
-
-
 existe : List Int -> Int -> Bool
 existe lista nro =
     if isEmpty lista then
@@ -311,16 +205,12 @@ existe lista nro =
 
 
 -- Une 2 listas pasadas por parámetros pero ignora los repetidos
-
-
 unirOfSet : List Int -> List Int -> List Int
 unirOfSet lista otraLista =
     removerDuplicados (concatenar lista otraLista)
 
 
 -- Función auxiliar para remover duplicados de una lista
-
-
 removerDuplicados : List Int -> List Int
 removerDuplicados lista =
     if isEmpty lista then
@@ -336,30 +226,60 @@ removerDuplicados lista =
             h :: resto
 
 
+-- Función auxiliar para transformar listas de listas
+transformarListas : List (List Int) -> (List Int -> List Int) -> List (List Int)
+transformarListas listas fx =
+    if isEmpty listas then
+        []
+    else
+        fx (head listas) :: transformarListas (tail listas) fx
+
+
+-- Función auxiliar para concatenar listas de listas
+concatenarListas : List (List Int) -> List (List Int) -> List (List Int)
+concatenarListas lista1 lista2 =
+    if isEmpty lista1 then 
+        lista2
+    else 
+        (head lista1) :: concatenarListas (tail lista1) lista2
+
+
 -- OPCIONAL: Subconjuntos
--- Dada una lista de enteros, retorna una lista con todos los posibles subconjuntos
--- Por ejemplo: [1,2,3] -> [[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
-
-
 subSets : List Int -> List (List Int)
 subSets lista =
-    case lista of
-        [] ->
-            [ [] ]
+    if isEmpty lista then
+        [ [] ]
+    else
+        let
+            x = head lista
+            xs = tail lista
+            subsetsXs = subSets xs
+            withX = transformarListas subsetsXs (\subset -> x :: subset)
+        in
+        concatenarListas subsetsXs withX
 
-        x :: xs ->
-            let
-                subsetsXs = subSets xs
-                withX = transformar subsetsXs (\subset -> x :: subset)
-            in
-            concatenar subsetsXs withX
+
+-- Función auxiliar para tomar los primeros n elementos de una lista
+tomar : Int -> List a -> List a
+tomar n lista =
+    if n <= 0 || isEmpty lista then
+        []
+    else
+        head lista :: tomar (n - 1) (tail lista)
+
+
+-- Función auxiliar para saltar los primeros n elementos de una lista
+saltar : Int -> List a -> List a
+saltar n lista =
+    if n <= 0 then
+        lista
+    else if isEmpty lista then
+        []
+    else
+        saltar (n - 1) (tail lista)
 
 
 -- OPCIONAL: Cortar
--- Dada una lista de enteros y un número entero n, retorna subconjuntos con n elementos
--- Ejemplo: [1,2,3,4,5] y 2 -> [[1,2], [3,4], [5]]
-
-
 cortar : List Int -> Int -> List (List Int)
 cortar lista n =
     if n <= 0 || isEmpty lista then
@@ -372,34 +292,7 @@ cortar lista n =
         chunk :: cortar resto n
 
 
--- Función auxiliar para tomar los primeros n elementos de una lista
-
-
-tomar : Int -> List a -> List a
-tomar n lista =
-    if n <= 0 || isEmpty lista then
-        []
-    else
-        head lista :: tomar (n - 1) (tail lista)
-
-
--- Función auxiliar para saltar los primeros n elementos de una lista
-
-
-saltar : Int -> List a -> List a
-saltar n lista =
-    if n <= 0 then
-        lista
-    else if isEmpty lista then
-        []
-    else
-        saltar (n - 1) (tail lista)
-
-
 -- Ejemplos de uso y funciones de prueba
--- Podés usar estos para probar tus implementaciones
-
-
 ejemplos : List String
 ejemplos =
     [ "max [1,2,3,4,5] debería devolver 5"
